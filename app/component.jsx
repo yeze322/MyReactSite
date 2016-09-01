2cf3d34c743ef23ffe1fd39375284a302e746f64
Ze Ye <zeye@microsoft.com>
2016-09-01 11:51:32 +0800

    init

---
+++ app/component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/antd.css'
+import TL from './tl'
+
+export default class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+      <Button type="primary" size="large">Primary</Button>
+      <h1>Hello world</h1>
+      <TL />
+      </div>
+    );
+  }
+}
