41640c45b0dd23cba89814d50bd46bf2889315c1
Ze Ye <zeye@microsoft.com>
2016-09-14 18:09:30 +0800

    add fetch my github API

--- components/tabs/ApiDoc.jsx
+++ components/tabs/ApiDoc.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
+import { RaisedButton } from 'material-ui'
 
 export default class ApiDoc extends React.Component {
+
+  state = {
+    json: ''
+  }
+
+  _fetchGit = () => {
+    var req = new XMLHttpRequest()
+    req.open('GET', 'https://api.github.com/users/yeze322/repos', false)
+    req.send(null)
+    this.setState({
+      json: req.responseText
+    })
+  }
+
+  _fetchGitAsync = () => {
+    var req = new XMLHttpRequest()
+    req.onreadystatechange = () => {
+      if(req.readyState == 4 && req.status == 200){
+        this.setState({
+          json: req.responseText
+        })
+      }
+    }
+    req.open('GET', 'https://api.github.com/users/yeze322/repos')
+    req.send(null)
+  }
 
   render() {
     return (
       <div>
+        <RaisedButton label="Fetch Github" primary={true} onClick={this._fetchGitAsync} />
         <h2>Github Link</h2>
         <p>https://github.com/yeze322/MyNodeApi.git</p>
+        {this.state.json}
       </div>
     )
   }
