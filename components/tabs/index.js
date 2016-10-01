3f1388034b5b0ccf12d25f01e07763fbea7977fd
Ze Ye <zeye@microsoft.com>
2016-10-01 18:38:44 +0800

    use react-router to manage tabs

---
+++ components/tabs/index.js
@@ -0,0 +1,7 @@
+export { default as Login } from './Login.jsx'
+export { default as ApiDoc } from './ApiDoc.jsx'
+export { default as Voters } from './Voters.jsx'
+export { default as WorkRecord } from './WorkRecord.jsx'
+export { default as Material } from './SideBar.jsx'
+
+export { default as TabUrlMap } from './TabUrlMap.jsx'
